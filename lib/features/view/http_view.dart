7f525c553013c535ba4a610b932485ba83e14cd7
hakannpeker <pekerhakan5@gmail.com>
2022-02-22 12:32:53 +0300

    http added

--- lib/features/view/http_view.dart
+++ lib/features/view/http_view.dart
@@ -43,72 +43,94 @@ class _HttpViewState extends State<HttpView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColor.backgroundColor,
-      bottomNavigationBar: const CustomBottomNavigationBar(),
-      body:isLoading ? SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-            Padding(
-              padding: AppPadding.inboxtitlePadding,
-              child: Text(
-                AppString.homeText,
-                textAlign: TextAlign.left,
-                style: Theme.of(context).textTheme.headline3?.copyWith(fontWeight: FontWeight.bold, color: AppColor.inboxTitleColor),
-              ),
-            ),
-            _sizedBoxHeight,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text(
-                  AppString.subText,
-                  style: Theme.of(context).textTheme.headline5?.copyWith(color: AppColor.subColor),
-                ),
-                const CustomElevatedButton()
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemCount: _todoModel?.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                      child: ListTile(
-                    title: Padding(
-                      padding: context.verticalPaddingLow,
-                      child: Text(
-                        _todoModel?[index].title.toString() ?? "",
-                        style: Theme.of(context).textTheme.titleLarge?.copyWith(color: AppColor.cardTitleColor),
-                      ),
-                    ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(_todoModel?[index].id.toString() ?? "",
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium
-                                ?.copyWith(decoration: TextDecoration.underline, color: AppColor.cardSubtitleColor))
-                      ],
-                    ),
-                    trailing: Container(
-                      decoration: BoxDecoration(color: AppColor.deleteContainerColor, borderRadius: context.lowBorderRadius),
-                      padding: context.paddingLow,
-                      child: const Icon(
-                        Icons.delete,
-                        color: AppColor.deleteColor,
-                      ),
-                    ),
-                  ));
-                },
-              ),
-            ),
-          ],
-        ),
-      ):Center(child: CircularProgressIndicator())
+        backgroundColor: AppColor.backgroundColor,
+        bottomNavigationBar: const CustomBottomNavigationBar(),
+        body: isLoading ? _scrollView(context) : progressIndicator());
+  }
+
+  SingleChildScrollView _scrollView(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: _columnOfScroll(context),
     );
   }
+
+  Column _columnOfScroll(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: AppPadding.inboxtitlePadding,
+          child: _mainTitle(context),
+        ),
+        _sizedBoxHeight,
+        _rowButtonAndText(context),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: _listviewBuilder(),
+        ),
+      ],
+    );
+  }
+
+  Text _mainTitle(BuildContext context) {
+    return Text(
+      AppString.homeText,
+      textAlign: TextAlign.left,
+      style: Theme.of(context).textTheme.headline3?.copyWith(fontWeight: FontWeight.bold, color: AppColor.inboxTitleColor),
+    );
+  }
+
+  Row _rowButtonAndText(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Text(
+          AppString.subText,
+          style: Theme.of(context).textTheme.headline5?.copyWith(color: AppColor.subColor),
+        ),
+        const CustomElevatedButton()
+      ],
+    );
+  }
+
+  ListView _listviewBuilder() {
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemCount: _todoModel?.length,
+      itemBuilder: (context, index) {
+        return _buildCard(context, index);
+      },
+    );
+  }
+
+  Card _buildCard(BuildContext context, int index) {
+    return Card(
+        child: ListTile(
+      title: Padding(
+        padding: context.verticalPaddingLow,
+        child: Text(
+          _todoModel?[index].title.toString() ?? "",
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(color: AppColor.cardTitleColor),
+        ),
+      ),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(_todoModel?[index].id.toString() ?? "",
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(decoration: TextDecoration.underline, color: AppColor.cardSubtitleColor))
+        ],
+      ),
+      trailing: Container(
+        decoration: BoxDecoration(color: AppColor.deleteContainerColor, borderRadius: context.lowBorderRadius),
+        padding: context.paddingLow,
+        child: const Icon(
+          Icons.delete,
+          color: AppColor.deleteColor,
+        ),
+      ),
+    ));
+  }
 }
+
+Center progressIndicator() => Center(child: CircularProgressIndicator());
